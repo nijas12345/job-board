c64eb0d529eaf7a0285fde1342c06d44c3ef8f5e
Nijas ma <nijazz1@gmail.com>
2025-08-09 13:20:59 +0530

    fix(backend): resolve CORS issue by updating middleware settings

--- backend/server.ts
+++ backend/server.ts
@@ -4,9 +4,13 @@ import dataBaseConnection from './config/mongodb'
 import jobRouter from './routes/jobRoute'
 import cors from 'cors'
 dotenv.config();
+
 const app = express();
 app.use(express.json());
-app.use(cors())
+const allowedOrigins = process.env.CORS_ORIGINS?.split(',');
+app.use(cors({
+  origin: allowedOrigins,
+}));
 dataBaseConnection()
 if(!process.env.PORT) throw new Error("Missing PORT environment variable");
 const PORT: number = parseInt(process.env.PORT,10);
